be6933f3d78d91f5fac6cc3711c87305f8017dda
Eric Herot <eric.github@herot.com>
2015-02-09 19:33:51 -0500

    Version bump 1.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_nat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          '1.1.1'
 
 depends 'et_fog'
