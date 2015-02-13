5e1e8ca852d090dacf0ff5093a9299c068826b37
Eric Herot <eric.github@herot.com>
2015-02-13 16:41:10 -0500

    Test that recipe successfully disables source/dest check

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,5 +1,26 @@
 # Encoding: utf-8
 require 'spec_helper'
+require 'fog'
+require 'net/http'
+
+describe 'instance settings' do
+  before do
+    network_interface_mac = Net::HTTP.get(
+      '169.254.169.254',
+      '/latest/meta-data/network/interfaces/macs/'
+    )
+    network_interface_id = Net::HTTP.get(
+      '169.254.169.254',
+      '/latest/meta-data/network/interfaces/macs/' \
+      "#{network_interface_mac}interface-id/")
+    connection = Fog::Compute::AWS.new(use_iam_profile: true)
+    @eni = connection.network_interfaces.get(network_interface_id)
+  end
+
+  it 'should have source-dest check disabled' do
+    expect(@eni.source_dest_check).to be false
+  end
+end
 
 describe 'iptables' do
   describe command('/sbin/iptables -t nat --list') do
