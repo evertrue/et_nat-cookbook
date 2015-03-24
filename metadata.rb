7825e42dad6c5c1b41e6a50ce9643c1f05a1772f
Eric Herot <eric.github@herot.com>
2015-03-23 20:07:27 -0400

    Give the cookbook a proper open source license

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'et_nat'
 maintainer       'EverTrue, Inc.'
 maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
-description      'Installs/Configures et_nat'
+description      'Apache v2.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.10'
 
