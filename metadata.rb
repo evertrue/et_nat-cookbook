47ba63a7b75bfaf017b76b6a232f787e4714178e
Eric Herot <eric.github@herot.com>
2015-06-03 14:59:02 -0400

    Version bump 1.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'Apache v2.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.10'
+version          '1.2.0'
 
 depends 'et_fog'
