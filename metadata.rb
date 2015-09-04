5de32a74cde15fc7cda675d4c0f0110249d205a1
Jeff Byrnes <jeff@evertrue.com>
2015-09-04 11:23:03 -0400

    Update maintainer email in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'et_nat'
 maintainer       'EverTrue, Inc.'
-maintainer_email 'eric.herot@evertrue.com'
+maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Apache v2.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
