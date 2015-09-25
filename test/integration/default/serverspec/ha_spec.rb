c057491871b5d598919f334acd24a18106d9665e
Jeff Byrnes <jeff@evertrue.com>
2015-09-25 12:10:27 -0400

    Add setting of heartbeat monitor URLs
    Makes use of a feature added in nat-monitor v1.1.0, which has the
    monitor send a heartbeat style ping to 1 or more URLs.
    
    Modeled after Cronitor.io’s run/complete/fail style URLs
    
    Changeset:
    https://github.com/evertrue/nat-monitor/compare/v1.0.9...v3.0.1

--- test/integration/default/serverspec/ha_spec.rb
+++ test/integration/default/serverspec/ha_spec.rb
@@ -10,4 +10,18 @@ describe 'et_nat::ha' do
     it { is_expected.to be_running }
     its(:args) { should match(/nat-monitor/) }
   end
+
+  describe file '/etc/nat_monitor.yml' do
+    describe '#content' do
+      subject { super().content }
+      it { is_expected.to include 'mocking: true' }
+      it { is_expected.to include 'aws_access_key_id: AWS_ACCESS_KEY_ID' }
+      it { is_expected.to include 'aws_secret_access_key: AWS_SECRET_ACCESS_KEY' }
+      it { is_expected.to include 'aws_url: http://192.168.1.191:5000' }
+      it { is_expected.to include 'route_table_id: rtb-a1b2c3d4' }
+      it { is_expected.to include 'run: http://example.com/run' }
+      it { is_expected.to include 'complete: http://example.com/complete' }
+      it { is_expected.to include 'fail: http://example.com/fail' }
+    end
+  end
 end
