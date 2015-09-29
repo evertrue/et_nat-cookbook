146c1667e0dc5458e1a8b9ecd0ee69d622d2803c
Jeff Byrnes <jeff@evertrue.com>
2015-09-29 13:38:44 -0400

    Bump to v2.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Apache v2.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.1'
+version          '2.0.2'
 
 depends 'et_fog'
 depends 'magic', '~> 1.1'
